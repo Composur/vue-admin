332e5282e9c26d9fd50296808985a92b5b361245
Composure <xutong1107@gmail.com>
2020-03-22 21:37:09 +0800

    pref:前端分页组件抽取

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,52 @@
+<template>
+<el-pagination :current-page='currentPage' :page-size="pageSize" :total="total" @size-change="handleSizeChange" @current-change='handleCurrentChange' background layout="prev, pager, next">
+</el-pagination>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    limit: {
+      type: Number,
+      default: 5,
+    },
+    total: {
+      type: Number,
+      default: 0,
+      required: true
+    }
+  },
+  computed: {
+    currentPage: {
+      get() {
+        return this.page
+      },
+      set(val) {
+        this.$emit('update:page', val)
+      },
+    },
+    pageSize: {
+      get() {
+        return this.limit
+      },
+      set(val) {
+        this.$emit('update:limit', val)
+      },
+    }
+  },
+  methods: {
+    handleCurrentChange(currentPage) {
+      this.currentPage = currentPage
+    },
+    handleSizeChange(pageSize) {
+      this.pageSize = pageSize;
+    },
+
+  }
+}
+</script>
