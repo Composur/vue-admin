4c7d05844f4148d6fe107972099dbab44fd2ba9c
Composure <xutong1107@gmail.com>
2020-05-28 21:05:34 +0800

    fix:index

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,131 +1,85 @@
 <template>
-<div class="dashboard-container">
-  <div class="dashboard-text">name: {{ name }}</div>
-  <div>
-    <button @click="addNode">Add Node</button>
-    <vue-tree-list @click="onClick" @change-name="onChangeName" @delete-node="onDel" @add-node="onAddNode" :model="data" default-tree-node-name="new node" default-leaf-node-name="new leaf" v-bind:default-expanded="false">
-      <span class="icon" slot="addTreeNodeIcon">📂</span>
-      <span class="icon" slot="addLeafNodeIcon">＋</span>
-      <span class="icon" slot="editNodeIcon">📃</span>
-      <span class="icon" slot="delNodeIcon">✂️</span>
-      <span class="icon" slot="leafNodeIcon">🍃</span>
-      <span class="icon" slot="treeNodeIcon">🌲</span>
-    </vue-tree-list>
-    <button @click="getNewTree">Get new tree</button>
-    <pre>
-    {{newTree}}
-    </pre>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div>{{es}}</div>
   </div>
-</div>
 </template>
 
 <script>
-import {
-  mapGetters
-} from 'vuex'
-import {
-  VueTreeList,
-  Tree,
-  TreeNode
-} from 'vue-tree-list'
+import { mapGetters } from "vuex";
+import { VueTreeList, Tree, TreeNode } from "vue-tree-list";
+import "babel-polyfill"
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   components: {
     VueTreeList
   },
   data() {
     return {
-      newTree: {},
-      data: new Tree([{
-          name: 'Node 1',
-          id: 1,
-          pid: 0,
-          dragDisabled: true,
-          addTreeNodeDisabled: true,
-          addLeafNodeDisabled: true,
-          editNodeDisabled: true,
-          delNodeDisabled: true,
-          children: [{
-            name: 'Node 1-2',
-            id: 2,
-            isLeaf: true,
-            pid: 1
-          }]
-        },
-        {
-          name: 'Node 2',
-          id: 3,
-          pid: 0,
-          disabled: true
-        },
-        {
-          name: 'Node 3',
-          id: 4,
-          pid: 0
-        }
-      ])
-    }
+      newTree: {}
+    };
   },
   methods: {
     touchStart(id) {
-      const dom = document.getElementById(id)
-      console.log(dom)
+      const dom = document.getElementById(id);
+      console.log(dom);
     },
     onDel(node) {
-      console.log(node)
-      node.remove()
+      console.log(node);
+      node.remove();
     },
 
     onChangeName(params) {
-      console.log(params)
+      console.log(params);
     },
 
     onAddNode(params) {
-      console.log(params)
+      console.log(params);
     },
 
     onClick(params) {
-      console.log(params)
+      console.log(params);
     },
 
     addNode() {
       var node = new TreeNode({
-        name: 'new node',
+        name: "new node",
         isLeaf: false
-      })
-      if (!this.data.children) this.data.children = []
-      this.data.addChildren(node)
+      });
+      if (!this.data.children) this.data.children = [];
+      this.data.addChildren(node);
     },
     getNewTree() {
-      var vm = this
+      var vm = this;
 
       function _dfs(oldNode) {
-        var newNode = {}
+        var newNode = {};
 
         for (var k in oldNode) {
-          if (k !== 'children' && k !== 'parent') {
-            newNode[k] = oldNode[k]
+          if (k !== "children" && k !== "parent") {
+            newNode[k] = oldNode[k];
           }
         }
 
         if (oldNode.children && oldNode.children.length > 0) {
-          newNode.children = []
+          newNode.children = [];
           for (var i = 0, len = oldNode.children.length; i < len; i++) {
-            newNode.children.push(_dfs(oldNode.children[i]))
+            newNode.children.push(_dfs(oldNode.children[i]));
           }
         }
-        return newNode
+        return newNode;
       }
 
-      vm.newTree = _dfs(vm.data)
-    },
+      vm.newTree = _dfs(vm.data);
+    }
   },
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(["name"]),
+    es:function(){
+      // return this.newTree.test?.test
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
