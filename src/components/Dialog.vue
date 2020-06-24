d9e5b6c96d448c1e8bab0dd965b400b1f155cda0
Composur <xutong1107@gmail.com>
2020-06-24 21:14:22 +0800

    feat:Dialog

---
+++ src/components/Dialog.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-dialog :visible.sync="visibleDialog">
+    <!-- 下面的写法可以传递组件内部的所有事件属性 -->
+  <!-- <el-dialog :visible.sync="visibleDialog" v-bind="$props" v-on="$listeners"> -->
+    <!--内容区域的默认插槽-->
+    <slot></slot>
+    <!--使用弹框的footer插槽添加按钮-->
+    <template #footer>
+      <!--对外继续暴露footer插槽，有个别弹框按钮需要自定义-->
+      <slot name="footer">
+        <!--将取消与确定按钮集成到内部-->
+        <span>
+          <el-button @click="$_handleCancel">取 消</el-button>
+          <el-button type="primary" @click="$_handleConfirm">确 定</el-button>
+        </span>
+      </slot>
+    </template>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name:'ComDialog',
+  props: {
+    // 对外暴露visible属性，用于显示隐藏弹框
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    // 通过计算属性，对.sync进行转换，外部也可以直接使用visible.sync
+    visibleDialog: {
+      get() {
+        return this.visible;
+      },
+      set() {
+        this.$emit("update:visible");
+      }
+    }
+  },
+  methods: {
+    // 对外抛出cancel事件
+    $_handleCancel() {
+      this.$emit("cancel");
+    },
+    // 对外抛出 confirm事件
+    $_handleConfirm() {
+      this.$emit("confirm");
+    }
+  }
+};
+</script>
---
+++ src/views/showCom/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>通用Dialog</span>
+        <el-button style="float: right; padding: 3px 0" type="text" @click="dialog">操作按钮</el-button>
+      </div>
+      <div class="text item">通过 props $emit 将按钮封装到组件内部，就可以不用重复去写确定和取消事件的代码</div>
+    </el-card>
+    <ComDialog :visible.sync="visibleDialog" @cancel="cancels" @confirm="confirms">
+      <div>dialog</div>
+    </ComDialog>
+  </div>
+</template>
+<script>
+import ComDialog from "@/components/Dialog";
+export default {
+  name: "Components",
+  data() {
+    return {
+      visibleDialog: false
+    };
+  },
+  components: {
+    ComDialog
+  },
+  methods: {
+    dialog() {
+      this.visibleDialog = true;
+    },
+    cancels() {
+      this.visibleDialog = false;
+    },
+    confirms() {
+      this.visibleDialog = false;
+    }
+  }
+};
+</script>
+<style>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
